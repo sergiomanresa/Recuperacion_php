91529b93f3aed3896c6b7f76bc60c8630f78abd6
sergiomanresa <sergiojmb8@gmail.com>
2023-06-11 13:55:00 +0200

    añadiendo estilo

--- php/Editar_paciente.php
+++ php/Editar_paciente.php
@@ -67,7 +67,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/Style.css">
+    <link rel="stylesheet" href="../css/Style_crear.css">
     <title>Editar paciente</title>
 </head>
 
