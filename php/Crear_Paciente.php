5e7f1ada7a51ced92ecb079f4e0556578f63f666
sergiomanresa <sergiojmb8@gmail.com>
2023-06-11 13:15:28 +0200

    agregando estilo

--- php/Crear_Paciente.php
+++ php/Crear_Paciente.php
@@ -68,7 +68,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/Style.css">
+    <link rel="stylesheet" href="../css/Style_crear.css">
     <title>Crear paciente</title>
 </head>
 
